bb4e5012c4122047cc1e70752e73a8338cc353b9
del0123 <detlefbrune@gmail.com>
2026-09-03 21:29:31 +0200

    Created http.server & .ServeMux plus html file

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting server...")
+
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
+	fmt.Println("Server stopped.")
 }
